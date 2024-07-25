f9f10048fe4780d551f822bf86e587e2d2ffc175
deril1307 <derilwijdan346@gmail.com>
2024-07-25 21:21:13 +0700

    menambahkan html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.getElementById("exploreButton")?.addEventListener("click", function () {
-  alert("Terima kasih telah mengklik tombol ini!");
-});
+// function scrollToSection(sectionId) {
+//   document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+// }
+
+// document.getElementById("menu-btn").addEventListener("click", function () {
+//   const menu = document.getElementById("mobile-menu");
+//   menu.classList.toggle("hidden");
+// });
+
+// function toggleMenu() {
+//   const menu = document.getElementById("mobile-menu");
+//   menu.classList.add("hidden");
+// }
+
+document.getElementById("menu-btn").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+}
+
+function scrollToSection(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+}
